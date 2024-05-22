93b0248daebe245c63d3640cb8a70818b6b812e2
shubhamchasing <shubhamchasing@gmail.com>
2024-05-22 19:07:42 +0530

    feat: Make logo a home link

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,18 +8,18 @@ function NavBar() {
   return (
     <Navbar className="navbar" size ="sm" style={{height:"10vh"}}>
       <Container fluid>
-        <img className="trello-logo"
-          src={
-            "https://a.trellocdn.com/prgb/dist/images/header-logo-spirit.d947df93bc055849898e.gif"
-          }
-          alt=""
-        />
+        <Link className="navbar-home" to="/">
+          <img
+            className="trello-logo w-25"
+            src={
+              "https://a.trellocdn.com/prgb/dist/images/header-logo-spirit.d947df93bc055849898e.gif"
+            }
+            alt=""
+          />{" "}
+        </Link>
         <Link className="navbar-home" to="/">
           Home
         </Link>
-
-
-      
       </Container>
     </Navbar>
   );
